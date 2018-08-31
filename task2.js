6a4790690041e2c8643254bbfbb8e7daa8c01ce6
C-Shi <cshi@ualberta.ca>
2018-08-31 17:41:05 -0600

    take input from command line

---
+++ task2.js
@@ -0,0 +1,18 @@
+function maxBottle(cash){
+    var total = cap = bottle = Math.floor(Math.floor(cash) / 2);
+    var newPop = 0;
+    newPop = total;
+    function calBottle(){
+        newPop = Math.floor(bottle / 2) + Math.floor(cap / 4);
+        cap = newPop + cap % 4;
+        bottle = newPop + bottle % 2;
+        total += newPop;
+        if (bottle < 2 && cap < 4) return 0;
+        return total + calBottle()
+    }
+
+    calBottle();
+    console.log(total);
+}
+
+maxBottle(process.argv[2] || 0); // add || as fallback in case user did not enter anything from command line
